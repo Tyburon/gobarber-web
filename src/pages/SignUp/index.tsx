d34384f23541fed40d305122f9a15663ae730170
Gabriel Cezar <gabriel.cezar.coder@gmail.com>
2020-09-21 22:21:30 -0300

    firstCommit

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,70 @@
+// eslint-disable-next-line no-use-before-define
+import React, { useCallback, useRef } from 'react';
+import { FiArrowLeft, FiMail, FiUser, FiLock, FiPrinter } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
+import getValidationErrors from '../../utils/getValidationErrors';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Container, Content, Background } from './styles';
+
+import Input from '../../components/input';
+import Button from '../../components/button';
+
+const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome é obrigatório'),
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um E-mail Valido'),
+        password: Yup.string().min(6, 'No mínimo 6 caracteres'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      const errors = getValidationErrors(err);
+
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
+
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <h1>Faça seu Logon</h1>
+
+          <Input name="name" icon={FiUser} placeholder="Nome" />
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
+
+          <Button type="submit">Cadastrar</Button>
+        </Form>
+        <a href="login">
+          <FiArrowLeft />
+          Voltar para logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
+
+export default SignUp;
---
+++ src/utils/getValidationErrors.ts
@@ -0,0 +1,17 @@
+import { useDebugValue } from 'react';
+
+import { ValidationError } from 'yup';
+
+interface Errors {
+  [key: string]: string;
+}
+
+export default function getValidationErrors(err: ValidationError): Errors {
+  const ValidationErrors: Errors = {};
+
+  err.inner.forEach(error => {
+    ValidationErrors[error.path] = error.message;
+  });
+
+  return ValidationErrors;
+}
